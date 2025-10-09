f3ecc09442746bbb22b35c949d6ce623771eae03
iBlack14 <yoset.hc.02@gmail.com>
2025-10-09 02:42:03 -0500

    Update next.config.mjs
    Panrl

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // ← ESTA ES LA LÍNEA CRÍTICA
   eslint: {
     ignoreDuringBuilds: true,
   },
